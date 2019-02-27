d49756ace9c5e1a80f122439c68beb5ab3c11886
Griffin Shoemaker <>
2019-02-27 11:43:43 -0600

    Worked on badge

--- cs310-team-project/src/cs310/team/project/Badge.java
+++ cs310-team-project/src/cs310/team/project/Badge.java
@@ -2,5 +2,27 @@
 package cs310.team.project;
 
 public class Badge {
-    
+           private String description;
+	   private String id;
+	
+	   public Badge(){}
+           
+           
+	   public Badge(String description, String id){
+		this.description = description;
+		this.id = id;
+	}
+	
+	   public String getDescription() {return description;}
+	   public String getId() {return id;}
+
+
+	   public void setDescription(String description) { this.description = description;}
+	   public void setId(String id) { this.id = id;}
+
+
+	   @Override
+	   public String toString(){
+		return "#" + getId() + " (" + getDescription() + ")";
+	}
 }
