aeadd59d0a9bbd654b6ff4e20759cca065c5854f
wmurphey <wmurphey@stu.jsu.edu>
2019-03-01 11:56:58 -0600

    Manual upload

--- cs310-team-project/src/cs310/team/project/Shift.java
+++ cs310-team-project/src/cs310/team/project/Shift.java
@@ -1,7 +1,150 @@
 
 package cs310.team.project;
 
+import java.time.LocalTime;
+
 public class Shift {
+   
+    private int id;
+    private String description;
+    private LocalTime start;
+    private LocalTime stop;
+    private int interval;
+    private int graceperiod;
+    private int dock;
+
+    private LocalTime lunchstart;
+    private LocalTime lunchstop;
+
+    private int lunchdeduct;
     
-    // Zaria Coprich
+    public Shift () {
+        
+       id = interval = graceperiod = dock = lunchdeduct = 0;
+       description = "";
+       
+       start = LocalTime.now();
+       stop = LocalTime.now();
+       
+       lunchstart = LocalTime.now();
+       lunchstop = LocalTime.now();
+ 
+    }
+    
+    public void setShiftStart(int hour, int minute) {
+        
+        start = LocalTime.of(hour, minute, 0);
+        
+    }
+    
+    // implement setShiftStop, setLunchStart, and setLunchStop here (follow the pattern shown above)
+    
+    
+    
+    public int getShiftStartHour() {
+        return (start.getHour());
+    }
+    
+    public int getShiftStartMinute() {
+        return (start.getMinute());
+    }
+    
+    // Follow the pattern shown above to provide getters for the hours/minutes for the other LocalTime fields
+    
+    public void setShiftStop(int hour, int minute) {
+        
+        stop = LocalTime.of(hour, minute, 0);
+        
+    }
+    
+    public int getShiftStopHour() {
+        return (stop.getHour());
+    }
+    
+    public int getShiftStopMinute() {
+        return (stop.getMinute());
+    }
+    
+    public void setShiftLunchStart(int hour, int minute) {
+        
+        stop = LocalTime.of(hour, minute, 0);
+        
+    }
+    
+    public int getShiftLunchStartHour() {
+        return (lunchstart.getHour());
+    }
+    
+    public int getShiftLunchStartMinute() {
+        return (stop.getMinute());
+    }
+    
+    public void setShiftLunchStop(int hour, int minute) {
+        
+        stop = LocalTime.of(hour, minute, 0);
+        
+    }
+    
+    public int getShiftLunchStopHour() {
+        return (lunchstart.getHour());
+    }
+    
+    public int getShiftLunchStopMinute() {
+        return (stop.getMinute());
+    }
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getInterval() {
+        return interval;
+    }
+
+    public void setInterval(int interval) {
+        this.interval = interval;
+    }
+
+    public int getGraceperiod() {
+        return graceperiod;
+    }
+
+    public void setGraceperiod(int graceperiod) {
+        this.graceperiod = graceperiod;
+    }
+
+    public int getDock() {
+        return dock;
+    }
+
+    public void setDock(int dock) {
+        this.dock = dock;
+    }
+
+    public int getLunchdeduct() {
+        return lunchdeduct;
+    }
+
+    public void setLunchdeduct(int lunchdeduct) {
+        this.lunchdeduct = lunchdeduct;
+    }
+
+    @Override
+    public String toString() {
+        return "Shift{" + "id=" + id + ", description=" + description + ", start=" + start + ", stop=" + stop + ", interval=" + interval + ", graceperiod=" + graceperiod + ", dock=" + dock + ", lunchstart=" + lunchstart + ", lunchstop=" + lunchstop + ", lunchdeduct=" + lunchdeduct + '}';
+    }
+     
 }
--- cs310-team-project/src/cs310/team/project/TASDatabase.java
+++ cs310-team-project/src/cs310/team/project/TASDatabase.java
@@ -1,6 +1,270 @@
+package tas_fa18;
 
-package cs310.team.project;
+import java.sql.*;
 
-public class TASDatabase {
+
+ public class TASDatabase {
+     
+     private Connection conn;
+
+     
+     public TASDatabase (){
+        try{
+             String server = ("jdbc:mysql://localhost:3306/tas");
+             String username = "team_B";
+             String password = "cs310_B";
+             
+             System.out.println("Connecting to " + server + "...");
+             
+             Class.forName("com.mysql.jdbc.Driver").newInstance();
+             
+             conn = DriverManager.getConnection(server, username, password);
+             
+             if(conn.isValid(0)){
+                 System.out.println("Connected Successfully!");
+             
+             }
+        }catch (Exception e) { 
+            System.err.println(e.toString());
+        } 
+         
+     }
     
+     public Badge getBadge(String badgeID){
+     
+         PreparedStatement pstSelect = null, pstUpdate = null;
+         ResultSet resultset = null;
+         
+         String query;
+         
+         boolean hasresults;
+         
+         Badge b = new Badge();
+         
+         try{
+      
+            query = "SELECT * FROM badge b";
+            pstSelect = conn.prepareStatement(query);
+            System.err.println("Submitting Query ... ");
+
+            hasresults = pstSelect.execute();
+
+            System.err.println("Getting Results ...");
+            
+            while (hasresults || pstSelect.getUpdateCount() != -1){
+
+                if (hasresults){
+
+                   resultset = pstSelect.getResultSet();
+                   
+                   if (resultset.next()){
+                        b.setID(resultset.getString("id"));
+                        b.setDescription(resultset.getString("description"));
+                   }
+                  
+            }
+             
+            hasresults = pstSelect.getMoreResults();
+
+            System.out.println();
+            conn.close();
+            }
+                 
+            }catch (Exception e){
+             System.err.println(e.toString());
+         }
+         
+         finally{
+             if(resultset != null){ try {resultset.close(); resultset = null;} catch (Exception e){}}
+             
+             if(pstSelect != null){ try {pstSelect.close(); pstSelect = null;} catch (Exception e){}}
+             
+             if(pstUpdate != null) { try {pstUpdate.close(); pstUpdate = null;} catch (Exception e){}}
+             
+         }
+         return b;  
+     }
+     
+     public Punch getPunch(int punchID){ 
+         
+         PreparedStatement pstSelect = null, pstUpdate = null;
+         ResultSet resultset = null;
+         
+         String query;
+         
+         boolean hasresults;
+         
+         Punch p = new Punch();
+         
+         try{
+      
+            query = "SELECT * FROM punch p";
+            pstSelect = conn.prepareStatement(query);
+            System.err.println("Submitting Query ... ");
+
+            hasresults = pstSelect.execute();
+
+            System.err.println("Getting Results ...");
+            
+            while (hasresults || pstSelect.getUpdateCount() != -1){
+
+                if (hasresults){
+
+                   resultset = pstSelect.getResultSet();
+                  
+                   if (resultset.next()){
+                        p.setID(resultset.getInt("id"));
+                        p.setTerminalID(resultset.getString("terminalid"));
+                        p.setBadgeID(resultset.getString("badgeid"));
+                        p.setOriginalTimeStamp(resultset.getString("originaltimestamp"));
+                        p.setPunchTypeID(resultset.getString("punchtypeid"));
+                   }
+                   
+   
+            }
+             
+            hasresults = pstSelect.getMoreResults();
+
+            System.out.println();
+            conn.close();
+            }
+                 
+            }catch (Exception e){
+             System.err.println(e.toString());
+         }
+         
+         finally{
+             if(resultset != null){ try {resultset.close(); resultset = null;} catch (Exception e){}}
+             
+             if(pstSelect != null){ try {pstSelect.close(); pstSelect = null;} catch (Exception e){}}
+             
+             if(pstUpdate != null) { try {pstUpdate.close(); pstUpdate = null;} catch (Exception e){}}
+             
+         }
+         
+         return p;
+     }
+     
+     public Shift getShift(int shiftID){
+         
+         PreparedStatement pstSelect = null, pstUpdate = null;
+         ResultSet resultset = null;
+         
+         String query;
+         
+         boolean hasresults;
+         
+         Shift s = new Shift();
+         
+         try{
+      
+            query = "SELECT * FROM shift s";
+            pstSelect = conn.prepareStatement(query);
+            System.err.println("Submitting Query ... ");
+
+            hasresults = pstSelect.execute();
+
+            System.err.println("Getting Results ...");
+            
+            while (hasresults || pstSelect.getUpdateCount() != -1){
+
+                if (hasresults){
+
+                   resultset = pstSelect.getResultSet();
+                  
+                   if(resultset.next()){
+                        s.setID(resultset.getInt("id"));
+                        s.setDescription(resultset.getString("description"));
+                        s.setStartTime(resultset.getString("start"));
+                        s.setStopTime(resultset.getString("stop"));
+                        s.setInterval(resultset.getString("interval"));
+                        s.setGracePeriod(resultset.getString("graceperiod"));
+                        s.setDock(resultset.getString("dock"));
+                        s.setLunchStart(resultset.getString("lunchstart"));
+                        s.setLunchStop(resultset.getString("lunchstop"));
+                        s.setLunchDeduct(resultset.getString("lunchdeduct"));
+                   
+                   }
+            }
+                     
+            
+            hasresults = pstSelect.getMoreResults();
+
+
+            System.out.println();
+            conn.close();
+            }
+                 
+            }catch (Exception e){
+             System.err.println(e.toString());
+         }
+         
+         finally{
+             if(resultset != null){ try {resultset.close(); resultset = null;} catch (Exception e){}}
+             
+             if(pstSelect != null){ try {pstSelect.close(); pstSelect = null;} catch (Exception e){}}
+             
+             if(pstUpdate != null) { try {pstUpdate.close(); pstUpdate = null;} catch (Exception e){}}
+             
+         }
+         return s;
+     }
+    public Shift getShift(Badge badge){
+        PreparedStatement pstSelect = null, pstUpdate = null;
+        ResultSet resultset = null;
+        
+        boolean hasresults;
+        String id = badge.getId();
+        int shiftID = -1; //****
+        Shift s = null;
+        
+        try{
+            String query = "SELECT * FROM employee WHERE badgeid = " + id + "";
+        
+            pstSelect = conn.prepareStatement(query);
+            System.err.println("Submitting Query ... ");
+
+            hasresults = pstSelect.execute();
+
+            System.err.println("Getting Results ...");
+            
+            while (hasresults || pstSelect.getUpdateCount() != -1){
+
+                if (hasresults){
+
+                   resultset = pstSelect.getResultSet();
+                  
+                   if(resultset.next()){
+                       
+                       shiftID = resultset.getInt("shiftid");
+                       s = getShift(shiftID);
+                   }
+                   
+                  
+            }
+                     
+            
+            hasresults = pstSelect.getMoreResults();
+
+
+            System.out.println();
+            conn.close();
+            }
+                 
+            }catch (Exception e){
+             System.err.println(e.toString());
+         }
+         
+         finally{
+             if(resultset != null){ try {resultset.close(); resultset = null;} catch (Exception e){}}
+             
+             if(pstSelect != null){ try {pstSelect.close(); pstSelect = null;} catch (Exception e){}}
+             
+             if(pstUpdate != null) { try {pstUpdate.close(); pstUpdate = null;} catch (Exception e){}}
+             
+         }
+         return s;
+        }
 }
+
+
